bedb20d82faddfb545acc182e583540c44f6c0fb
최준형 <k14414@naver.com>
2024-05-09 13:23:20 +0900

    완성

--- list.cpp
+++ list.cpp
@@ -1,3 +1,17 @@
 #include "list.h"
-
+#include "linked_list.h"
 // TODO: List 클래스 구현 작성
+
+
+int get(int index)
+{
+    LinkedList::get(index);
+}
+void insert(int index, int value)
+{
+    LinkedList::insert(index, value);
+}
+void remove(int index)
+{
+    LinkedList::remove(index);
+}
